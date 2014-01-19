833b6a73f2c670a633bbb09c7a3d54f5bc476361
Jeff <jeff.revesz@buzzfeed.com>
2014-01-18 21:51:31 -0500

    pep8

--- operative/models/__init__.py
+++ operative/models/__init__.py
@@ -5,11 +5,14 @@ ATTR_AS_INT = lambda x: int(x)
 ATTR_AS_FLOAT = lambda x: float(x)
 ATTR_AS_DATETIME = lambda x: datetime.datetime.strptime(x, '%Y-%m-%d')
 
+
 class OperativeModel(object):
+
     """
     Parent class for all operative models
 
     """
+
     def __init__(self, report_row):
         attribs = {self.REPORT_MAPPINGS[k]: v for k, v in report_row.items() if k in self.REPORT_MAPPINGS}
         for k, v in attribs.items():
@@ -25,6 +28,7 @@ class OperativeModel(object):
 
 
 class Advertiser(OperativeModel):
+
     """
     Represents an Operative Advertiser.
 
@@ -36,6 +40,7 @@ class Advertiser(OperativeModel):
 
 
 class Order(OperativeModel):
+
     """
     Represents an Operative order.
 
@@ -47,13 +52,15 @@ class Order(OperativeModel):
 
     ATTR_TRANSLATIONS = {'id': ATTR_AS_INT,
                          'start_date': ATTR_AS_DATETIME,
-                         'end_date': ATTR_AS_DATETIME,}
+                         'end_date': ATTR_AS_DATETIME, }
 
     def __init__(self, report_row):
         OperativeModel.__init__(self, report_row)
         self.advertiser = Advertiser(report_row)
 
+
 class LineItem(OperativeModel):
+
     """
     Represents an Operative Line Item.
 
@@ -66,14 +73,14 @@ class LineItem(OperativeModel):
                        'Quantity': 'quantity',
                        'Unit Cost': 'unit_cost',
                        'Cost Type': 'cost_type',
-                       'Cost': 'cost',}
+                       'Cost': 'cost', }
 
     ATTR_TRANSLATIONS = {'id': ATTR_AS_INT,
                          'start_date': ATTR_AS_DATETIME,
                          'end_date': ATTR_AS_DATETIME,
                          'quantity': ATTR_AS_INT,
                          'unit_cost': ATTR_AS_FLOAT,
-                         'cost': ATTR_AS_FLOAT,}
+                         'cost': ATTR_AS_FLOAT, }
 
     def __init__(self, report_row):
         OperativeModel.__init__(self, report_row)
