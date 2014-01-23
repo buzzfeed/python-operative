710993cf77c31f0153967b86f77659a457c53fd8
Jeff <jeff.revesz@buzzfeed.com>
2014-01-23 14:12:41 -0500

    bumped version to 0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 
 setup(
     name='python-operative',
-    version='0.1.0',
+    version='0.1.1',
     author='Jane Kelly & Jeff Revesz',
     author_email='jane.kelly@buzzfeed.com',
     packages=find_packages(),
